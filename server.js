51e2a52c2f5c3ab02aee3346f60a24123a0e983f
Juan Zamudio <jzamudio14@gmail.com>
2024-06-16 01:18:46 -0400

    adding server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const { createServer } = require('http')
+const { createServer } = require('https')
 const { parse } = require('url')
 const next = require('next')
  
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = '34.139.134.197'
-const port = 3000
+const hostname = 'pocket-farmer-pmpbu5qgca-ue.a.run.app'
+const port = 8080
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
